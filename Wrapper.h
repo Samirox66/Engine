da2885e9e97fc2fc37f316af4c5bbc0014f02e14
Samir <bsam@bk.ru>
2022-11-14 16:22:17 +0300

    first commit

---
+++ Wrapper.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Wrapper
+{
+public:
+private:
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Engine.h"
+#include "Wrapper.h"
+
+int main(int argc, char** argv)
+{
+	std::cout << "good" << std::endl;
+	return 0;
+}
